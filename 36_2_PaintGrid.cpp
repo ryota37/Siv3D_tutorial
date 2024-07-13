8ccac864c17867c9775f48396d5479ee2c19ba42
ryota37 <ryota.nakahama@access-company.com>
2024-07-13 22:41:35 +0900

    (WIP)36_2use Grid to hold color info.

--- 36_2_PaintGrid.cpp
+++ 36_2_PaintGrid.cpp
@@ -6,21 +6,30 @@ void Main()
 
 	// ここを Array<std::pair<Rect, Color>> squares; にすれば、色を後から変更できる？
 	Array<Rect> squares;
+	Grid<int> colors(8,6,0);
 
 	while (System::Update())
 	{
 
 		for (int i=0; i<8; i++) {
 			for (int j=0; j<6; j++) {
-				squares << Rect{i*100,j*100,100}.draw(Palette::White);
+				if (colors[j][i]==0) {
+					Color color = Palette::White;
+				} else if (colors[j][i]==1) {
+					Color color = Palette::Lightgray;
+				} else if (colors[j][i]==2) {
+					Color color = Palette::Dimgray;
+				} else if (colors[j][i]==3) {
+					Color color = Palette::Black;
+				} 
+				squares << Rect{i*100,j*100,100}.draw(color);				
 			}
 		}
 
 		for (const auto& square : squares) {
 			if(square.leftClicked()){
-				// squareのPaletteをBlackに書き換えたい
-				// square.draw(Palette::Black); だけだと、次のフレームに変更が維持されない
-				
+				// squareがsquaresの中でどの座標のものなのかを知る必要がある
+				colors[j][i]++ ;	
 			}
 		}
 
