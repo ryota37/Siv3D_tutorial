8777a336e9f69b7965280047e7d2c7e260cc535a
ryota37 <ryota.nakahama@access-company.com>
2024-07-16 12:26:07 +0900

    (WIP)36_3 single ball moves in random direction

--- 36_3_BouceBalls.cpp
+++ 36_3_BouceBalls.cpp
@@ -3,14 +3,14 @@
 void Main()
 {
 	Scene::SetBackground(Palette::Mediumseagreen);
-	Circle circle {100,100,30};\
-	double theta = 0.33 * Math::Pi ;
+	Circle circle {100,100,30};
+	Vec2 vec = RandomVec2();
 
 	while (System::Update())
 	{
 		const double t = Scene::Time();
-		circle.x += (10*cos(0.5)*t);
-		circle.y += (10*sin(0.5)*t);
+		circle.x += (10*vec.x*t);
+		circle.y += (10*vec.y*t);
 		circle.draw();
 
 	}
