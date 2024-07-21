f5d7f9435c7871e45df7743a7c34709e2f3f5cc9
ryota37 <ryota.nakahama@access-company.com>
2024-07-21 20:10:16 +0900

    (WIP)36_3 fix the ball speed

--- 36_3_BouceBalls.cpp
+++ 36_3_BouceBalls.cpp
@@ -4,35 +4,27 @@ void Main()
 {
 	Scene::SetBackground(Palette::Mediumseagreen);
 	Circle circle {100,100,30};
-	Vec2 vec = RandomVec2();
+	Vec2 vec = RandomVec2()*1000;
 
 	while (System::Update())
 	{
-		const double t = Scene::Time();
+		const double t = Scene::DeltaTime();
 
-		if (circle.x < 0 || circle.x > 800)
+		if (circle.x < 50 || circle.x > 750)
 		{
 			vec.x *= -1;
 		} else {
 			vec.x = vec.x;
 		}
-		circle.x += (10*vec.x*t);
+		circle.x += (vec.x*t);
 
-		if (circle.y < 0 || circle.y > 600)
+		if (circle.y < 50 || circle.y > 550)
 		{
 			vec.y *= -1;
 		} else {
 			vec.y = vec.y;
 		}
-		circle.y += (10*vec.y*t);
-
-		// 壁に当たった時、反射する実装
-		// ぶつかった方向の速度をマイナスにする
-		// 例えば、右の壁にぶつかったら、x方向の速度をマイナスにする
-		// 線分(or極細の長方形)と円の接触判定を見て、左右の壁の場合x方向を、上下の壁の場合y方向をマイナスにする
-		// もしくは、circle.xが0か800になったらvec.xをマイナスにする実装にする
-		// 0~800ではなく、円の半径を考慮して、0+半径~800-半径にしてもいいかも
-
+		circle.y += (vec.y*t);
 
 		circle.draw();
 
