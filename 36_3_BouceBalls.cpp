d7c11ead13574dedb61bbfe36e674d7289567350
ryota37 <ryota.nakahama@access-company.com>
2024-07-25 20:13:44 +0900

    (WIP)36_3 lower the ball speed to avoid a bug

--- 36_3_BouceBalls.cpp
+++ 36_3_BouceBalls.cpp
@@ -4,25 +4,21 @@ void Main()
 {
 	Scene::SetBackground(Palette::Mediumseagreen);
 	Circle circle {100,100,30};
-	Vec2 vec = RandomVec2()*1000;
+	Vec2 vec = RandomVec2(200);
 
 	while (System::Update())
 	{
 		const double t = Scene::DeltaTime();
 
-		if (circle.x < 50 || circle.x > 750)
+		if (circle.x <= 30 || circle.x >= Scene::Width()-30)
 		{
-			vec.x *= -1;
-		} else {
-			vec.x = vec.x;
+			vec.x *= -1.0;
 		}
 		circle.x += (vec.x*t);
 
-		if (circle.y < 50 || circle.y > 550)
+		if (circle.y <= 30 || circle.y >= Scene::Height()-30)
 		{
-			vec.y *= -1;
-		} else {
-			vec.y = vec.y;
+			vec.y *= -1.0;
 		}
 		circle.y += (vec.y*t);
 
