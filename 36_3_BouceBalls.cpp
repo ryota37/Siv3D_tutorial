995b5298127b6b04998d23c80435b917ad2e3f38
ryota37 <ryota.nakahama@access-company.com>
2024-07-27 18:38:05 +0900

    36_3 implement multiple balls

--- 36_3_BouceBalls.cpp
+++ 36_3_BouceBalls.cpp
@@ -3,26 +3,39 @@
 void Main()
 {
 	Scene::SetBackground(Palette::Mediumseagreen);
-	Circle circle {100,100,30};
-	Vec2 vec = RandomVec2(200);
+	struct Ball {
+		Vec2 position;
+		Vec2 speed;
+	};
+
+	const double radius = 30.0;
+
+	Array<Ball> balls;
+
+	for (int i=0; i<5; i++) {
+		balls << Ball{Vec2{100,100}, RandomVec2(200)};
+	}
 
 	while (System::Update())
 	{
 		const double t = Scene::DeltaTime();
 
-		if (circle.x <= 30 || circle.x >= Scene::Width()-30)
-		{
-			vec.x *= -1.0;
+		for (auto& ball : balls) {
+			if (ball.position.x <= 30 || ball.position.x >= Scene::Width()-30)
+			{
+				ball.speed.x *= -1.0;
+			}
+			ball.position.x += (ball.speed.x*t);
+
+			if (ball.position.y <= 30 || ball.position.y >= Scene::Height()-30)
+			{
+				ball.speed.y *= -1.0;
+			}
+			ball.position.y += (ball.speed.y*t);
 		}
-		circle.x += (vec.x*t);
 
-		if (circle.y <= 30 || circle.y >= Scene::Height()-30)
-		{
-			vec.y *= -1.0;
+		for (const auto& ball : balls) {
+			Circle{ ball.position, radius }.draw();
 		}
-		circle.y += (vec.y*t);
-
-		circle.draw();
-
 	}
 }
