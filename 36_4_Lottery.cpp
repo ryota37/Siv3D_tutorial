3e43868353d62c0b4e05422998565c44145c7019
ryota37 <ryota.nakahama@access-company.com>
2024-07-29 09:37:20 +0900

    (WIP)36_4 random destination stream

--- 36_4_Lottery.cpp
+++ 36_4_Lottery.cpp
@@ -4,6 +4,12 @@ void Main()
 {
     Scene::SetBackground(Palette::Mediumseagreen);
 
+    String destination = U"Paris";
+    const Array<String> destinations = 
+    {
+        U"Paris", U"New York", U"Tokyo", U"London", U"Sydney", U"Rome", U"Seoul", U"Beijing", U"Moscow", U"Cairo"
+    };
+
     while(System::Update())
     {
         // ランダムに行き先を表示するアプリだと思われる
@@ -11,6 +17,9 @@ void Main()
             // (Copilotが出してくれた都市名セット) Paris, New York, Tokyo, London, Sydney, Rome, Seoul, Beijing, Moscow, Cairo
         // Stopというボタンもあるので、それを押すと止まる？
 
-        SimpleGUI::Button(U"Stop", Vec2(300, 300));
+        destination = destinations.choice();
+
+        SimpleGUI::Button(destination, Vec2{400,200});
+        SimpleGUI::Button(U"Stop", Vec2(400, 400));
     }
 }
